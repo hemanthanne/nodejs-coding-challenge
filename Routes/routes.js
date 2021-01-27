d9e17f7fc79ee68284d6f7c99226604b991a6b7b
hemanthanne <72768101+hemanthanne@users.noreply.github.com>
2021-01-27 10:04:39 -0600

    Delete Routes directory

--- Routes/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../Controllers/Controller");
-router.post("/AddUser", controller.AddUser);
-router.put("/UpdateUser", controller.UpdateUser);
-router.delete("/DeleteUser", controller.DeleteUser);
-router.get("/GetUsers", controller.GetUsers);
-
-module.exports = router;
